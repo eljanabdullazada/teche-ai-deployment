881269285c59f02d88280062f8121c469a9e4977
eljan_abdullazada <a3371977@gmail.com>
2026-07-07 20:30:01 +0400

    change code in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,5 @@ app = FastAPI(title="AI Starter Kit Backend")
 @app.get("/health")
 def health():
     return {
-        "salamla": "Xosh gelmisiniz",
-        "status": "ok"
+        "salamlama": "Welcome to the AI starter Kit backend!"
     }
