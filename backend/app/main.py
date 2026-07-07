ece8e4be6734e8d01a0df6c9d8b02489e01673a1
eljan_abdullazada <a3371977@gmail.com>
2026-07-07 17:53:03 +0400

    welcome the users

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,4 +4,7 @@ app = FastAPI(title="AI Starter Kit Backend")
 
 @app.get("/health")
 def health():
-    return {"status": "ok"}
+    return {
+        "salamlama": "Welcome AI Starter Kit Backend",
+        "status": "ok"
+    }
