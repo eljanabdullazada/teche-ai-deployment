89e47a1eac77a2161f37b98155bc7888ed656a63
eljan_abdullazada <a3371977@gmail.com>
2026-07-08 20:37:01 +0400

    change return message in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,5 +5,5 @@ app = FastAPI(title="AI Starter Kit Backend")
 @app.get("/health")
 def health():
     return {
-        "sayhi": "Welcome to the AI starter modified July 8"
+        "welcome_message": "Second welcome message"
     }
