b2fdfca0753580f6f370da6f9d7898240cc34308
eljan_abdullazada <a3371977@gmail.com>
2026-07-13 15:20:30 +0400

    change deploy yml

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,5 +5,5 @@ app = FastAPI(title="AI Starter Kit Backend")
 @app.get("/health")
 def health():
     return {
-        "wlms": "Welcome July 13 test 1"
+        "wlms": "Welcome July 13 test 2"
     }
